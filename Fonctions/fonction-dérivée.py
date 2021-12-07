0ceca35209616b9832b021ad63fbbf76c32d9276
Aymeric DE LANGE <aymericdelange80@gmail.com>
2021-12-07 10:56:33 +0100

    Add - Fonction dérivée

---
+++ Fonctions/fonction-dérivée.py
@@ -0,0 +1,8 @@
+#   f(x) = -0,005x² + 0,6 - 14
+#   f'(x) = -0,010x + 0,6
+
+a = float(input("Saisir la valeur de [ax²] "))
+b = float(input("Saisir la valeur de [bx] "))
+c = float(input("Saisir la valeur de [c] "))
+
+print("f'(x) = ", a * 2,"+", b)
